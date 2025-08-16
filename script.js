e809a96b79b8c6fb753a08c732e6176a3ff02512
Gul_Khuban <gkhuban44@gmail.com>
2025-08-16 23:34:08 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/* 🔹 Page 1 JS (index.html) */
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.title.includes("Destinations")) {
+    console.log("Page 1 (Destinations) loaded!");
+  }
+});
